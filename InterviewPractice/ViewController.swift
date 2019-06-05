c636f51381eeb3f7e0c825b155cd2116090fcc2f
alexladines <alexladines@outlook.com>
2019-06-05 09:03:40 -0700

    Create project.

---
+++ InterviewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterviewPractice
+//
+//  Created by Alex Ladines on 6/5/19.
+//  Copyright © 2019 Alex Ladines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Interview Questions
+
+This is my personal guide that I will use as I take walks throughout the day.
